bbf2651d1bcfc177205f313e4ae66ef5a3b748ea
Kevin Jonathan .AK <kevinak2007@gmail.com>
2026-02-14 13:04:51 +0530

    Roman to integers

---
+++ Roman_to_Integer/RomantoInteger.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def romanToInt(self, s):
+        roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        total = 0
+        prev = 0
+        for i in range(len(s) - 1, -1, -1):
+            current= roman[s[i]]
+            if current < prev:
+                total -= current
+            else:
+                total += current
+            prev=current
+        return total
